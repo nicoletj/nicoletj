eb1fee86f420dbbecf57fe6c1a766e87522c20be
nicoletj <43450672+nicoletj@users.noreply.github.com>
2018-10-21 21:04:27 +0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	
-var countDownDate = new Date("Dec 16, 2018 13:20:00").getTime();
+var countDownDate = new Date("December 16, 2018 13:20:00 GMT+02:00").getTime();
 
 var x = setInterval(myTimer, 1000);
 
@@ -17,21 +17,22 @@ function myTimer() {
 	var hours = Math.floor((tj % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((tj % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((tj % (1000 * 60)) / 1000);
+	var weeks = (days % 7);
 
 	// Display the result in the element with id="demo"
 	document.getElementById("count-down").innerHTML = days + " Days " + hours + " Hours "
   + minutes + " Minutes " + seconds + " Seconds";
 	
-	document.getElementById("weeks").innerHTML = "Weeks: " + (days % 7);
+	document.getElementById("weeks-left").innerHTML = "Weeks: " + weeks;
 
 	// If the count down is finished, write some text 
 	if (days < 7) {
-		document.getElementById("weeks").innerHTML = "LAST WEEK!";
+		document.getElementById("weeks-left").innerHTML = "LAST WEEK!";
 	}
 	if (tj < 0) {
 		clearInterval(x);
 		document.getElementById("count-down").innerHTML = "TJ 0";
-		document.getElementById("weeks").innerHTML = "WEEKS: 0";
+		document.getElementById("weeks-left").innerHTML = "WEEKS: 0";
 	}
 }
 	
